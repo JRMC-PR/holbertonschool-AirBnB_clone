54c6b1413d5abf7ac3c0644e3fd886472e89564d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-30 16:07:24 -0400

    commit adds task 5 to the repo

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
@@ -31,6 +32,7 @@ class BaseModel:
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""

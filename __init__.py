8e5dec1a0153c6faf1369fd7d78a63b394bc0f1d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-31 08:19:06 -0400

    commit updates taks 5

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#!/ur/bin/python3"""
-"""This module creates a unique FileStorage instance for the application
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()

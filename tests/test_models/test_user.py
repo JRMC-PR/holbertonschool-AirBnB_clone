3d0ff77792de1c7355844c24f44b1399b42a4a1c
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-02 11:27:51 -0400

    Unit test for user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -31,6 +32,20 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.my_user2.email, "airbnb2@mail.com")
         self.assertEqual(self.my_user2.password, "root")
 
+    def test_inheritance(self):
+        """Test if User inherits from BaseModel"""
+        self.assertTrue(issubclass(type(self.my_user), BaseModel))
+
+    def test_str(self):
+        """Test the __str__ method"""
+        self.assertEqual(str(self.my_user), "[User] ({}) {}".
+                         format(self.my_user.id, self.my_user.__dict__))
+
+    def test_to_dict(self):
+        """Test the to_dict method"""
+        my_user_dict = self.my_user.to_dict()
+        self.assertEqual(type(my_user_dict), dict)
+        self.assertTrue("to_dict" in dir(self.my_user))
 
 if __name__ == '__main__':
     unittest.main()
